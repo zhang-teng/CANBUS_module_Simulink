430cf0aa0847b4328cfc7b95b25c0ed245446d2d
zhang-teng <zhangteng_1223@sina.com>
2014-06-11 19:59:48 +0800

    A test file for drive of CANBUS of CANET-200
    A test program to build module in simulink
    just for testing drive program

---
+++ test_can/test_can/main.cpp
@@ -0,0 +1,33 @@
+ï»¿//#pragma   comment(lib,"ControlCAN.lib") 
+#include <stdio.h>
+#include <Windows.h>
+//#include <stdint.h>
+//#include <BaseTsd.h>
+#include "ControlCAN.h"
+#include "CAN_control.h"
+//#pragma comment(lib,"ControlCAN.lib")
+int m_connect=0;//±íÊ¾ÏÖÓÐÁ´½Ó×´Ì¬ 1ÎªÁ¬½Ó
+
+void main()
+{	
+	char *info;
+	UINT ID=0x00000001;
+	UCHAR Data[8]="1234567";
+	BYTE DataLen=8;
+	CreateThread(NULL,0,(LPTHREAD_START_ROUTINE)monitor,NULL,0,NULL);
+//	Connect();
+	while(1)
+	{
+		if(m_connect==1)
+		{
+		Sleep(500);
+		sendinfo(ID,Data,DataLen);
+		}
+		else
+		{
+		Sleep(500);
+		}
+
+	}
+	
+}
